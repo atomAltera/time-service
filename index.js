7730493f20f3a068f562655c4b5a6af76d695fcc
Konstantin Alikhanov <atomAltera@gmail.com>
2019-09-22 12:43:19 +0400

    Fixing bugs

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
-const express = require("express");
-const {format: formatDate} = require('date-fns');
+const express = require('express')
+const { format: formatDate } = require('date-fns')
 
-const app = express();
+const app = express()
 
-
-const port = parseInt(process.env.PORT || "3000");
+const port = parseInt(process.env.PORT || '3000')
 
 app.use(
-    express.json()
+  express.json()
 )
 
-
-
 app.get('/current', (req, res) => {
-    const current = new Date();
+  const current = new Date()
 
-    const str = formatDate(current, "yyyy.MM.dd - HH:mm.ss");
+  const str = formatDate(current, 'yyyy.MM.dd - HH:mm.ss')
 
-    res.json({
-        str,
-        current
-    });
-}); 
-
+  res.json({
+    str,
+    current
+  })
+})
 
 app.listen(port, () => {
-    console.log("server started at port", port);
-});
+  console.log('server started at port', port)
+})
